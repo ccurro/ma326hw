0b24f40c5ec8a3854a127247a7f06ca06d13c01c
Chris <heinz1226@gmail.com>
2012-11-28 19:11:19 -0500

    hw8 init

---
+++ hw8/auto/hw8.el
@@ -0,0 +1,58 @@
+(TeX-add-style-hook "hw8"
+ (lambda ()
+    (TeX-add-symbols
+     '("M" 3)
+     '("mfrac" 2)
+     '("dete" 1)
+     '("dime" 1)
+     '("rank" 1)
+     '("tr" 1)
+     "tab"
+     "gap"
+     "T"
+     "U"
+     "V"
+     "W"
+     "bigzero")
+    (TeX-run-style-hooks
+     "fancyhdr"
+     "geometry"
+     "verbatim"
+     "appendix"
+     "microtype"
+     "siunitx"
+     "url"
+     "multirow"
+     "mathtools"
+     "gauss"
+     "booktabs"
+     "stmaryrd"
+     "amsfonts"
+     "amsthm"
+     "amssymb"
+     "amsmath"
+     "graphicx"
+     "enumerate"
+     "babel"
+     "english"
+     "inputenc"
+     "utf8"
+     "setspace"
+     "latex2e"
+     "art12"
+     "article"
+     "letterpaper"
+     "12pt"
+     "./qs/4"
+     "./qs/14"
+     "./qs/17"
+     "./qs/19"
+     "./qs/41"
+     "qs/3"
+     "qs/8"
+     "qs/12"
+     "qs/17"
+     "qs/2"
+     "qs/6"
+     "qs/7")))
+
